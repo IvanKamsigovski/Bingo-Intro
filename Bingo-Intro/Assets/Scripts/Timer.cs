8f46246382b6f97fb5d66d5592ff7a97eca1a7cc
IvanKamsigovski <ivankamsigovski2@gmail.com>
2020-08-24 12:48:47 +0200

    Timer

---
+++ Bingo-Intro/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public Slider slider;
+    public TMP_Text timerText;
+
+    float startingTime = 60f;
+    float currentTime = 0f;
+
+    void Start()
+    {
+        slider.maxValue = startingTime;
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        CountDown();
+    }
+
+    void CountDown()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+        slider.value = currentTime;
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
